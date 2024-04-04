30e80b460572b496d24df7e52339b8cf823bd9bd
coldnoodle_kjy <kjy11069@sookmyung.ac.kr>
2024-04-04 13:50:39 +0900

    feat: 24.04.04 update

--- codes/src/dp/boj_11053.java
+++ codes/src/dp/boj_11053.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//도저히 감이 안잡혀 https://st-lab.tistory.com/137 를 참고하여 공부했습니다
 public class boj_11053 {
-    static int[] dp;
+    static Integer[] dp;
     static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
         arr=new int[n];
-        dp=new int[n];
+        dp=new Integer[n];
 
         StringTokenizer st=new StringTokenizer(br.readLine()," ");
         for(int i=0;i<n;i++){
@@ -31,12 +32,12 @@ public class boj_11053 {
     }
 
     public static int recursion(int n){
-        if(dp[n]==null){
-            dp[n]=1;
+        if(dp[n]==null){//n번째 이전에 탐색한 결과물이 있는지 판단
+            dp[n]=1;//없었다면 1로 초기화
 
-            for(int i=n-1;i>=0;i--){
-                if(arr[i]<arr[n]){
-                    dp[n]=Math.max(dp[n],recursion(i)+1);
+            for(int i=n-1;i>=0;i--){//n-1부터 0까지 탐색 1
+                if(arr[i]<arr[n]){//현재 노드의 값(n번째 값)보다 작은 노드를 탐색
+                    dp[n]=Math.max(dp[n],recursion(i)+1);//+1 한 이유: n번째 원소 추가
                 }
             }
         }
