9079d3bc87a260723edf8bb5b5a0f6a3190f2eff
coldnoodle_kjy <kjy11069@sookmyung.ac.kr>
2024-04-30 23:07:39 +0900

    feat: 24.04.30 update

---
+++ codes/src/heap/boj_1927.java
@@ -0,0 +1,28 @@
+package heap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class boj_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> queue=new PriorityQueue<>();
+        int n=Integer.parseInt(br.readLine());
+
+        for(int i=0;i<n;i++){
+            int val=Integer.parseInt(br.readLine());
+
+            if(val==0){
+                if(queue.isEmpty()){
+                    System.out.println("0");
+                }else{
+                    System.out.println(queue.poll());
+                }
+            }else{
+                queue.add(val);
+            }
+        }
+    }
+}
