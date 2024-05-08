4a6a938a1deda55bf32fe4e137ff207c1961e7bb
coldnoodle_kjy <kjy11069@sookmyung.ac.kr>
2024-05-08 16:21:57 +0900

    feat: 24.05.08 update

---
+++ codes/src/sorting/boj_10867.java
@@ -0,0 +1,25 @@
+package sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_10867 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        int[] arr=new int[N];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        arr = Arrays.stream(arr).distinct().toArray();
+        Arrays.sort(arr);
+        for(int a: arr){
+            System.out.print(a+" ");
+        }
+    }
+}
