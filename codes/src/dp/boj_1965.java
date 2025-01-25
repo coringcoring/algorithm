c57edc0483cb52be5c0cee8847685e803f6ce9a8
coringcoring <kjy11069@sookmyung.ac.kr>
2025-01-25 21:12:20 +0900

    feat: 25.01.25 update

---
+++ codes/src/dp/boj_1965.java
@@ -0,0 +1,33 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1965 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int[] arr=new int[n];
+        int[] dp=new int[n];
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+            dp[i]=1;
+        }
+
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            max=Math.max(max,dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
